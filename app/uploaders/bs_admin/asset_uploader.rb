f43cd5580aa02c6547a48e9a2cc8ad751e24b6ac
Leo Uzon <leo.uz86@gmail.com>
2016-09-30 18:21:46 -0300

    ...

--- app/uploaders/bs_admin/asset_uploader.rb
+++ app/uploaders/bs_admin/asset_uploader.rb
@@ -7,7 +7,7 @@ class BsAdmin::AssetUploader < CarrierWave::Uploader::Base
   process :resize_to_limit => [700, 700]
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}".gsub('_', '-')
   end
 
   version :thumb do
